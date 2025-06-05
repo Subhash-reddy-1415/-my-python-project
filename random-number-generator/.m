1dd2eba88fc767c4b5a9d69f2047e7b865642715
Subhash-reddy-1415 <subhashreddy050@gmail.com>
2025-06-05 19:36:13 +0530

    Create .m

---
+++ random-number-generator/.m
@@ -0,0 +1,6 @@
+import random
+print("WELCOME TO RANDOM NUMBER GENERATOR")
+start_number=int(input("ENTER THE STARTING NUMBER="))
+end_number=int(input("ENTER THE ENDING NUMBER="))
+random_number=random.randint(start_number,end_number)
+print(random_number)
